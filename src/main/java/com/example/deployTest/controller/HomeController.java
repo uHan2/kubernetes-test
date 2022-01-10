db4e2f8d6e29deddedbc70cef63991ab11f62abc
AE210601 <asgd56@etoos.com>
2022-01-10 18:23:10 +0900

    refactor: edit url

--- src/main/java/com/example/deployTest/controller/HomeController.java
+++ src/main/java/com/example/deployTest/controller/HomeController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class HomeController {
 
-    @GetMapping("/tmp")
+    @GetMapping("/")
     public int homeController() {
         return HttpServletResponse.SC_ACCEPTED;
     }
